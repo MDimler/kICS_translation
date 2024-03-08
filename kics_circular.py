b62464b3780e0c2c165a103fd6f983c149a5339b
MDimler <113688502+MDimler@users.noreply.github.com>
2024-03-08 06:25:22 +0100

    Update kics_circular.py
    Adding of a docstring and final comentation

--- kics_circular.py
+++ kics_circular.py
@@ -6,8 +6,16 @@ Created on Wed Jan  4 10:55:45 2023
 """
 import numpy as np
 
-def circular(r_k_norm, varargin=""):
-    
+def circular(r_k_norm):
+    """This function circular averages the input matrix. 
+
+    Inputs:
+    r_k_norm:    2 or 3 dimensional complex numpy array that is going to be circular averaged.
+
+    Outputs:
+    r_k_circ:    1 or 2 dimensional complex numpy array that contains the circular average of input.
+    ksq:         1 dimensional float numpy array that contains the |k|^2-values corresponding to unique circular values.
+    """
     # compute size of input array over every dimension
     size_y = np.size(r_k_norm,0)
     size_x = np.size(r_k_norm,1)
@@ -41,27 +49,21 @@ def circular(r_k_norm, varargin=""):
     l_nums = len(lattice_nums)
     
     r_k_circ = np.zeros((l_nums,T),dtype=complex) # array to fill with circular average of r_k_norm
-    # unique_inds = np.zeros((l_nums,1))
 
     for i in range(len(lattice_nums)):
         n = lattice_nums[i]
         inds = np.where(int_lattice_sqrd == n) # indices in "int_lattice_sqrd" with value "n"
-        # subs_xy = np.unravel_index(inds, np.size(int_lattice_sqrd), "F")
-        # print(inds)
-        # print(subs_xy)
-        # subs_x = subs_xy[0][0]
-        # subs_y = subs_xy[0][1]
-        # print(subs_x, subs_y)
-        # unique_inds[i]=inds[0]
+
         l_inds = len(inds[0])
         for j in range(l_inds):
             y = inds[1][j]
             x = inds[0][j]
             try:
-                r_xy = np.reshape(r_k_norm[x,y,:], (1,T)) # Or r_k_norm[y,x,:]
+                r_xy = np.reshape(r_k_norm[x,y,:], (1,T))
             except:
-                r_xy = np.reshape(r_k_norm[x,y], (1,T)) # Or r_k_norm[y,x,:]
+                r_xy = np.reshape(r_k_norm[x,y], (1,T))
             r_k_circ[i,:] = r_k_circ[i,:] + r_xy/l_inds
 
+    # Create list of unique |k|^2-values.
     ksq = (2*np.pi)**2 / (size_x*size_y)**2 * lattice_nums
     return r_k_circ, ksq
