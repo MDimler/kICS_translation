c4bdce75b9f9495bc01044783e4ca03195c2c339
MDimler <113688502+MDimler@users.noreply.github.com>
2024-03-08 07:59:11 +0100

    Update kics_kicsSubNoise.py
    Adding a dockstring and final coments

--- kics_kicsSubNoise.py
+++ kics_kicsSubNoise.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 12 14:46:52 2023
+"""This module contains the function kICSSubNoise.
 
+Created on Thu Jan 12 14:46:52 2023
 @author: Martin
 """
-
-# Outputs noise subtracted extended kICS correlation at time lag 0. ksq_min
-# and ksq_max are large k-squared values, where the autocorrelation at
-# tau=0 has sufficiently decayed and can be considered as the contribution
-# from the noise. All k-squared values between [ksq_min,ksq_max] are
-# averaged and subtracted from the zero time-lag.
-
-# INPUT PARAMETERS
-
-# r_k:          output from kICS(...)
-# ksq_min:      minimum k-squared bound for averaging
-# ksq_max:      maximum k-squared bound for averaging
-
-# OUTPUT PARAMTERS
-
-# r_k_0_sub:    noise-subtracted extended kICS autocorrelation at tau=0
 
 import numpy as np
 
 from kics_getKSqVector import getKSqVector
 
 def kICSSubNoise(r_k,ksq_min,ksq_max):
+    """ This function gives the subtracted extended kICS correltaion at time lag 0.
+    
+    Inputs:
+    r_k:              2 or 3 dimensional array corresponding to the output from kICS
+    ksq_min:          Float corresponding to the minimum |k|^2 bound for averaging
+    ksq_max:          Float corresponding to the maximum |k|^2 bound for averaging
+    Outputs:
+    r_k_0_sub    :    noise-subtracted extended kICS autocorrelation at tau=0
+    """
     # get k-squared values between [ksq_min,ksq_max] for averaging
     noise_inds = getKSqVector(r_k, ('kSqMin', ksq_min, 'kSqMax', ksq_max), True)
     r_k_0 = r_k[:,:,0]
